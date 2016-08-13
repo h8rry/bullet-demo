8ad574f2a1ab4b0163b850e0171b6b963c0cecb2
h8rry <harry@harrychen.net>
2016-08-13 11:06:34 +0800

    different version of specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,21 +3,36 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   render_views
 
+  before(:each) do
+    @user_1 = User.create
+    @user_2 = User.create
+
+    20.times do
+      Question.create(title: 'random', user: @user_1)
+      Question.create(title: 'random', user: @user_2)
+    end
+
+    Bullet.start_request
+  end
+
+  after(:each) do
+    Bullet.perform_out_of_channel_notifications if Bullet.notification?
+    Bullet.end_request
+  end
+
   context "index all users, includes each user's questions" do
     it "responds correctly" do
-      user_1 = User.create
-      user_2 = User.create
+      expected_response = {
+        users: [{
+          questions: @user_1.questions
+        }, {
+          questions: @user_2.questions
+        }]
+      }
 
-      20.times do
-        Question.create(title: 'random', user: user_1)
-        Question.create(title: 'random', user: user_2)
-      end
-
-      Bullet.start_request
       get :index
       expect(response.code).to eq("200")
-      Bullet.perform_out_of_channel_notifications if Bullet.notification?
-      Bullet.end_request
+      expect(response.body).to eq(expected_response.to_json)
     end
   end
 end
